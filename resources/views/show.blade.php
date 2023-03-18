5ce621c49fae6ab48b02535194c9765e5e496395
Millonada <an.mil.es98@gmail.com>
2023-03-18 13:07:48 -0600

    Se termina la actividad

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,6 +4,6 @@
         <li class="list-group-item">Nombre: {{$sup->nombre}}</li>
         <li class="list-group-item">Apodo: {{$sup->apodo}}</li>
         <li class="list-group-item">Adicional: {{$sup->adicional}}</li>
-        <li class="list-group-item">Foto: <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Spider-Man.jpg/1200px-Spider-Man.jpg" alt="" width="100"></li>
+        <li class="list-group-item">Foto: <img src="{{$sup->img}}" alt="" width="100"></li>
       </ul>
 </x-app>
